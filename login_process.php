3e611b18992b96d73eb95d9789801c2097bb7577
techiehouse <pavannamana0611@gmail.com>
2024-03-01 23:39:30 +0530

    Updated

--- login_process.php
+++ login_process.php
@@ -10,11 +10,19 @@ $password = $_POST['password'];
 // Check if the submitted credentials match the valid credentials
 if ($username === $valid_username && $password === $valid_password) {
     // Successful login
-    echo "Login successful. Welcome, $username!";
+    echo '<script type="text/javascript">
+       alert("Login Successful! Please enter the candidate details.");
+       window.location = "dataEntry.php";
+    </script>';
     // Redirect to a welcome page or perform other actions here
 } else {
     // Failed login
-    echo "Invalid username or password. Please try again.";
+    // header("location:index.php");
+
+    echo '<script type="text/javascript">
+       alert("Invalid username or password. Please try again.");
+       window.location = "index.php";
+    </script>';
     // Redirect back to the login page or display an error message
 }
 ?>
